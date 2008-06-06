a694a0358566dac6a2c891ab76ba73dec3e118c9
shemnon <shemnon@a5544e8c-8a19-0410-ba12-f9af4593a198>
2008-06-06 01:29:50 +0000

    clean things up, add documentation, add license header
    git-svn-id: http://svn.codehaus.org/groovy/trunk/groovy/modules/griffon@12729 a5544e8c-8a19-0410-ba12-f9af4593a198

---
+++ src/commons/griffon/groovy/binding/BindPath.java
@@ -0,0 +1,172 @@
+/*
+ * Copyright 2008 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package griffon.groovy.binding;
+
+import org.codehaus.groovy.runtime.InvokerHelper;
+
+import java.beans.PropertyChangeListener;
+import java.util.Set;
+
+import groovy.lang.MetaClass;
+
+/**
+ * The bind path object.  This class represents one "step" in the bind path.
+ */
+@SuppressWarnings({"unchecked"}) // all are of type Object, so generics are useless
+public class BindPath {
+
+    /**
+     * The object we think we are bound to
+     */
+    Object currentObject;
+
+    /**
+     * The proeprty we are intiereted in
+     */
+    String propertyName;
+
+    /**
+     * Are we bound to the object-wide PropertyChangeListener?
+     */
+    boolean boundGlobal;
+
+    /**
+     * Are we bound to a property-specific PropertyChangeListner
+     */
+    boolean boundName;
+
+    /**
+     * The steps further down the path from us
+     */
+    BindPath[] children;
+
+    /**
+     * Called when we detect a change somewherer down our path.
+     * First, check to see if our object is changing.  If so remove our old listener
+     * Next, update the reference object the children have and recurse
+     * Finally, add listeners if we have a different object
+     *
+     * @param listener This listener to attach.
+     * @param newObject The object we should read our property off of.
+     * @param updateSet The list of objects we have added listeners to
+     */
+    public synchronized void updatePath(PropertyChangeListener listener, Object newObject, Set updateSet) {
+        if (currentObject != newObject) {
+            removeListeners(listener);
+        }
+        if ((children != null) && (children.length > 0)) {
+            try {
+                Object newValue = null;
+                if (newObject != null) {
+                    updateSet.add(newObject);
+                    newValue = InvokerHelper.getProperty(newObject, propertyName);
+                }
+                for (BindPath child : children) {
+                    child.updatePath(listener, newValue, updateSet);
+                }
+            } catch (Exception e) {
+                //LOGME
+                // do we ignore it, or fail?
+            }
+        }
+        if (currentObject != newObject) {
+            addListeners(listener, newObject, updateSet);
+        }
+    }
+
+    /**
+     * Adds all the listeners to the objects in the bind path.
+     * This assumes that we are not added as listeners to any of them, hence
+     * it is not idempotent.
+     *
+     * @param listener This listener to attach.
+     * @param newObject The object we should read our property off of.
+     * @param updateSet The list of objects we have added listeners to
+     */
+    public void addAllListeners(PropertyChangeListener listener, Object newObject, Set updateSet) {
+        addListeners(listener, newObject, updateSet);
+        if ((children != null) && (children.length > 0)) {
+            try {
+                Object newValue = null;
+                if (newObject != null) {
+                    updateSet.add(newObject);
+                    newValue = InvokerHelper.getProperty(newObject, propertyName);
+                }
+                //System.out.println("Considering " + newObject + " as value for " + propertyName);
+                for (BindPath child : children) {
+                    child.addAllListeners(listener, newValue, updateSet);
+                }
+            } catch (Exception e) {
+                //LOGME
+                // do we ignore it, or fail?
+            }
+        }
+    }
+
+    static final Class[] NAME_PARAMS = {String.class, PropertyChangeListener.class};
+    static final Class[] GLOBAL_PARAMS = {PropertyChangeListener.class};
+
+    /**
+     * Add listeners to a specific object.  Updates the bould flags and update set
+     *
+     * @param listener This listener to attach.
+     * @param newObject The object we should read our property off of.
+     * @param updateSet The list of objects we have added listeners to
+     */
+    public void addListeners(PropertyChangeListener listener, Object newObject, Set updateSet) {
+        boundName = false;
+        boundGlobal = false;
+        if (newObject != null) {
+            MetaClass mc = InvokerHelper.getMetaClass(newObject);
+            if (!mc.respondsTo(newObject, "addPropertyChangeListener", NAME_PARAMS).isEmpty()) {
+                InvokerHelper.invokeMethod(newObject, "addPropertyChangeListener", new Object[] {propertyName, listener});
+                //System.out.println("Bound to name " + propertyName + " on " + newObject);
+                boundName = true;
+                updateSet.add(newObject);
+            } else if (!mc.respondsTo(newObject, "addPropertyChangeListener", GLOBAL_PARAMS).isEmpty()) {
+                InvokerHelper.invokeMethod(newObject, "addPropertyChangeListener", listener);
+                //System.out.println("Bound  globally on " + newObject);
+                boundGlobal = true;
+                updateSet.add(newObject);
+            }
+        }
+        currentObject = newObject;
+    }
+
+    /**
+     * Remove listeners, believeing that our bould flags are accurate and it removes
+     * only as declared.
+     *
+     * @param listener This listener to attach.
+     */
+    public void removeListeners(PropertyChangeListener listener) {
+        if (boundGlobal) {
+            try {
+                InvokerHelper.invokeMethod(currentObject, "removePropertyChangeListener", listener);
+            } catch (Exception e) {
+                //LOGME ignore the failure
+            }
+        }
+        if (boundName) {
+            try {
+                InvokerHelper.invokeMethod(currentObject, "removePropertyChangeListener", new Object[] {propertyName, listener});
+            } catch (Exception e) {
+                //LOGME ignore the failure
+            }
+        }
+    }
+
+}
--- src/commons/griffon/groovy/binding/ClosureTriggerBinding.java
+++ src/commons/griffon/groovy/binding/ClosureTriggerBinding.java
@@ -1,15 +1,28 @@
+/*
+ * Copyright 2008 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package griffon.groovy.binding;
 
-import org.codehaus.groovy.binding.*;
-import org.codehaus.groovy.runtime.InvokerHelper;
-import org.codehaus.groovy.runtime.StackTraceUtils;
 import groovy.lang.Closure;
 import groovy.lang.GroovyObjectSupport;
-import groovy.lang.MetaClass;
+import org.codehaus.groovy.binding.*;
 
-import java.util.*;
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeEvent;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ClosureTriggerBinding implements TriggerBinding, SourceBinding {
 
@@ -23,11 +36,11 @@ public class ClosureTriggerBinding implements TriggerBinding, SourceBinding {
         this.closure = closure;
     }
 
-    private BindPath createBindPath(String propertyName, FullInterceptorProxy proxy) {
+    private BindPath createBindPath(String propertyName, BindPathSnopper proxy) {
         BindPath bp = new BindPath();
         bp.propertyName = propertyName;
         List<BindPath> childPaths = new ArrayList<BindPath>();
-        for (Map.Entry<String, FullInterceptorProxy> entry : proxy.fields.entrySet()) {
+        for (Map.Entry<String, BindPathSnopper> entry : proxy.fields.entrySet()) {
             childPaths.add(createBindPath(entry.getKey(), entry.getValue()));
         }
         bp.children = childPaths.toArray(new BindPath[childPaths.size()]);
@@ -38,24 +51,28 @@ public class ClosureTriggerBinding implements TriggerBinding, SourceBinding {
         if (source != this) {
             throw new RuntimeException("Source binding must the Trigger Binding as well");
         }
-        FullInterceptorProxy delegate = new FullInterceptorProxy();
+        BindPathSnopper delegate = new BindPathSnopper();
         try {
             // first, pick out the property references
-            synchronized (closure) {
+            final Closure closureLocalCopy = closure;
+            synchronized (closureLocalCopy) {
                 // be obsessive and asseret total ownership over the closure while being snopped at
-                Object oldDelegate = closure.getDelegate();
-                int oldResolveStrategy = closure.getResolveStrategy();
+                Object oldDelegate = closureLocalCopy.getDelegate();
+                int oldResolveStrategy = closureLocalCopy.getResolveStrategy();
 
                 try {
-                    closure.setDelegate(delegate);
-                    closure.setResolveStrategy(Closure.DELEGATE_ONLY);
-                    closure.call();
+                    closureLocalCopy.setDelegate(delegate);
+                    closureLocalCopy.setResolveStrategy(Closure.DELEGATE_ONLY);
+                    closureLocalCopy.call();
+                } catch (DeadEndException e) {
+                    // we want this exception exposed.
+                    throw e;
                 } catch (Exception e) {
                     //LOGME
                     // ignore it, likely failing because we are faking out properties
                 } finally {
-                    closure.setResolveStrategy(oldResolveStrategy);
-                    closure.setDelegate(oldDelegate);
+                    closureLocalCopy.setResolveStrategy(oldResolveStrategy);
+                    closureLocalCopy.setDelegate(oldDelegate);
                 }
             }
         } catch (Exception e) {
@@ -63,12 +80,11 @@ public class ClosureTriggerBinding implements TriggerBinding, SourceBinding {
             throw new RuntimeException("A closure expression binding could not be created because of " + e.getClass().getName() + ":\n\t" + e.getMessage());
         }
         List<BindPath> rootPaths = new ArrayList<BindPath>();
-        for (Map.Entry<String, FullInterceptorProxy> entry : delegate.fields.entrySet()) {
+        for (Map.Entry<String, BindPathSnopper> entry : delegate.fields.entrySet()) {
             BindPath bp =createBindPath(entry.getKey(), entry.getValue());
             bp.currentObject = closure;
             rootPaths.add(bp);
         }
-        //System.out.println(rootPaths);
         PropertyPathFullBinding fb = new PropertyPathFullBinding();
         fb.setSourceBinding(new ClosureSourceBinding(closure));
         fb.setTargetBinding(target);
@@ -81,197 +97,35 @@ public class ClosureTriggerBinding implements TriggerBinding, SourceBinding {
     }
 }
 
-class BindPath {
-    Object currentObject;
-    String propertyName;
-    boolean boundGlobal;
-    boolean boundName;
-
-    BindPath[] children;
-
-
-    public synchronized void updatePath(PropertyChangeListener listener, Object newObject, Set updateSet) {
-        if (currentObject != newObject) {
-            removeListeners(listener);
-        }
-        //System.out.println(children);
-        //System.out.println(children.length);
-        if ((children != null) && (children.length > 0)) {
-            try {
-                Object newValue = null;
-                if (newObject != null) {
-                    updateSet.add(newObject);
-                    newValue = InvokerHelper.getProperty(newObject, propertyName);
-                }
-                //System.out.println("Considering " + newObject + " as value for " + propertyName);
-                for (BindPath child : children) {
-                    child.updatePath(listener, newValue, updateSet);
-                }
-            } catch (Exception e) {
-                //LOGME
-                // do we ignore it, or fail?
-            }
-        }
-        if (currentObject != newObject) {
-            addListeners(listener, newObject, updateSet);
-        }
-    }
-
-    public void addAllListeners(PropertyChangeListener listener, Object newObject, Set updateSet) {
-        addListeners(listener, newObject, updateSet);
-        //System.out.println(children);
-        //System.out.println(children.length);
-        if ((children != null) && (children.length > 0)) {
-            try {
-                Object newValue = null;
-                if (newObject != null) {
-                    updateSet.add(newObject);
-                    newValue = InvokerHelper.getProperty(newObject, propertyName);
-                }
-                //System.out.println("Considering " + newObject + " as value for " + propertyName);
-                for (BindPath child : children) {
-                    child.addAllListeners(listener, newValue, updateSet);
-                }
-            } catch (Exception e) {
-                //LOGME
-                // do we ignore it, or fail?
-            }
-        }
-    }
-
-    static final Class[] NAME_PARAMS = {String.class, PropertyChangeListener.class};
-    static final Class[] GLOBAL_PARAMS = {PropertyChangeListener.class};
-
-    public void addListeners(PropertyChangeListener listener, Object newObject, Set updateSet) {
-        boundName = false;
-        boundGlobal = false;
-        if (newObject != null) {
-//            try {
-                MetaClass mc = InvokerHelper.getMetaClass(newObject);
-                if (!mc.respondsTo(newObject, "addPropertyChangeListener", NAME_PARAMS).isEmpty()) {
-                    InvokerHelper.invokeMethod(newObject, "addPropertyChangeListener", new Object[] {propertyName, listener});
-                    //System.out.println("Bound to name " + propertyName + " on " + newObject);
-                    boundName = true;
-                    updateSet.add(newObject);
-                } else if (!mc.respondsTo(newObject, "addPropertyChangeListener", GLOBAL_PARAMS).isEmpty()) {
-                    InvokerHelper.invokeMethod(newObject, "addPropertyChangeListener", listener);
-                    //System.out.println("Bound  globally on " + newObject);
-                    boundGlobal = true;
-                    updateSet.add(newObject);
-                } else {
-                    //System.out.println("Not Bound at all on " + newObject);
-                }
-//            } catch (Exception e) {
-//                // didn't bind to propery name for some reason
-//                // possibly because they don't have property scope change listeners
-//                    System.out.println(Arrays.asList(newObject.getClass().getDeclaredMethods()));
-//                    System.out.println(Arrays.asList(newObject.getClass().getDeclaredFields()));
-//                    //LOGME
-//                    // do we ignore it, or fail?
-//                }
-//            }
-        }
-        currentObject = newObject;
-    }
-
-    public void removeListeners(PropertyChangeListener listener) {
-        if (boundGlobal) {
-            try {
-                InvokerHelper.invokeMethod(currentObject, "removePropertyChangeListener", listener);
-            } catch (Exception e) {
-                //LOGME ignore the failure
-            }
-        }
-        if (boundName) {
-            try {
-                InvokerHelper.invokeMethod(currentObject, "removePropertyChangeListener", new Object[] {propertyName, listener});
-            } catch (Exception e) {
-                //LOGME ignore the failure
-            }
-        }
-    }
-
-    public String toString() {
-        return propertyName + " { " + Arrays.asList(children) + " ) ";
-    }
-}
-
-class PropertyPathFullBinding extends AbstractFullBinding implements PropertyChangeListener {
-
-    /**
-     * The set of all objects where a property change does incur a listener-re-check
-     */
-    Set updateObjects = new HashSet();
-    BindPath[] bindPaths;
-    boolean bound;
-
-    public void bind() {
-        //System.out.println("Binding!");
-        //Closure refClosure = ((ClosureSourceBinding)sourceBinding).getClosure();
-        updateObjects.clear();
-        //System.out.println("Update objects cleard");
-        for (BindPath bp : bindPaths) {
-            //System.out.println("Binding path " + bp);
-            bp.addAllListeners(this, bp.currentObject, updateObjects);
-        }
-        bound = true;
-    }
-
-    public void unbind() {
-        updateObjects.clear();
-        for (BindPath path : bindPaths) {
-            path.removeListeners(this);
-        }
-        bound = false;
-    }
-
-    public void rebind() {
-        if (bound) bind();
-    }
-
-    public void propertyChange(PropertyChangeEvent evt) {
-        //System.out.println(evt);
-        //System.out.println(updateObjects);                
-        if (updateObjects.contains(evt.getSource())) {
-            bind();
-        }
-        update();
-    }
+class DeadEndException extends RuntimeException {
+    DeadEndException(String message) { super(message); }
 }
 
 class DeadEndObject {
     public Object getProperty(String property) {
-        throw new RuntimeException("Cannot bind to a property on the return value of a method call");
+        throw new DeadEndException("Cannot bind to a property on the return value of a method call");
     }
     public Object invokeMethod(String name, Object args) {
         return this;
     }
 }
 
-class FullInterceptorProxy extends GroovyObjectSupport {
+class BindPathSnopper extends GroovyObjectSupport {
     static final DeadEndObject DEAD_END = new DeadEndObject();
 
-    Map<String, FullInterceptorProxy> fields = new LinkedHashMap<String, FullInterceptorProxy>();
+    Map<String, BindPathSnopper> fields = new LinkedHashMap<String, BindPathSnopper>();
 
     public Object getProperty(String property) {
         if (fields.containsKey(property)) {
-            //System.out.println("Faking property " + property + " with existing " + fields.get(property));
             return fields.get(property);
         } else {
-            FullInterceptorProxy proxy = new FullInterceptorProxy();
+            BindPathSnopper proxy = new BindPathSnopper();
             fields.put(property, proxy);
-            //System.out.println("Faking property " + property + " with new " + fields.get(property));
-            return proxy; 
+            return proxy;
         }
     }
 
     public Object invokeMethod(String name, Object args) {
-        //System.out.println("Faking method " + name + " with DeadEnd");
         return DEAD_END;
-    }
-
-
-    public String toString() {
-        return fields.toString();
     }
 }
