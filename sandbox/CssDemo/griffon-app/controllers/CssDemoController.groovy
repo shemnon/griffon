878a72b963620e4dd34935df0bc1cc82c47af128
shemnon <shemnon@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-02-07 04:56:22 +0000

    sandbox isn't in any dist package, no need to track it
    git-svn-id: https://svn.codehaus.org/griffon/core/branches/GRIFFON_0.1.x@459 bb902e69-0157-0410-a63b-ba7c3dd83122

--- sandbox/CssDemo/griffon-app/controllers/CssDemoController.groovy
+++
@@ -1,7 +0,0 @@
-class CssDemoController {
-
-    // these will be injected by Griffon
-    def model
-    def view
-
-}
--- sandbox/CssDemo/griffon-app/lifecycle/Initialize.groovy
+++
@@ -1,5 +0,0 @@
-import griffon.util.GriffonPlatformHelper
-import com.u2d.css4swing.CSSEngine
-
-CSSEngine.initialize()
-GriffonPlatformHelper.tweakForNativePlatform(app)
