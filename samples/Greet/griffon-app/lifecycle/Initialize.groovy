a356d711c314ba8b21f671c2d35374eacb449887
shemnon <shemnon@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-07-07 03:42:00 +0000

    GRIFFON-49 - in Greet limit redirects, so a bad login password doesn't get you blacklisted
    git-svn-id: https://svn.codehaus.org/griffon/core/branches/GRIFFON_0.1.x@813 bb902e69-0157-0410-a63b-ba7c3dd83122

--- samples/Greet/griffon-app/lifecycle/Initialize.groovy
+++ samples/Greet/griffon-app/lifecycle/Initialize.groovy
@@ -22,3 +22,4 @@ import groovy.swing.SwingBuilder
 
 GriffonPlatformHelper.tweakForNativePlatform(app)
 SwingBuilder.lookAndFeel('nimbus', 'mac', 'gtk', ['metal', [boldFonts: false]])
+System.properties['http.maxRedirects']='2'
